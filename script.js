f57ea17b781e958aaaebaa27ec96fee6a6feab42
bobob0311 <qweasdzxc0311@naver.com>
2024-04-01 19:53:55 +0900

    찢어진 사진 랜덤 배치

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const startButton = document.querySelector('button');
+const puzzleContainer = document.querySelector('.container');
+
+const uniqueNumbers = new Set();
+while (uniqueNumbers.size < 9) {
+    uniqueNumbers.add(Math.ceil(Math.random() * 9))    
+}
+const imageIndexArray = [...uniqueNumbers];
 
 startButton.addEventListener('click', function () {
     const gameScreen = document.querySelector('.game-screen');
@@ -6,5 +13,23 @@ startButton.addEventListener('click', function () {
 
     const startScreen = document.querySelector('.start-screen');
     startScreen.classList.add('hide');
+
+    const originalImage =document.getElementById('originalImage');
+    originalImage.setAttribute('src', "./data/image1/originalImage.png");
+
+    for (let i = 0; i < 9;i++) {
+        console.log(i);
+        const div = document.createElement('div');
+        div.classList.add('image-container');
+
+        const img = document.createElement('img');
+        img.setAttribute('src', `./data/image1/image${imageIndexArray[i]}.jpg`);
+        div.appendChild(img);
+
+        puzzleContainer.appendChild(div);
+    }
 });
+
+
+
 
