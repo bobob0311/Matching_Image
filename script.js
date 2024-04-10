19ed3410e23f4daaeebb1d23ff255d84f4480097
bosung si <qweasdzxc0311@naver.com>
2024-04-10 20:51:27 +0900

    제한시간 정하는 페이지 생성

--- script.js
+++ script.js
@@ -1,43 +1,25 @@
 import gameData from "./src/gameData.js";
 import { loadPuzzleImages, createImageContainer,handlePuzzleElementClick } from "./src/puzzle.js";
-import { resetGameData,createInterval } from "./src/utils.js";
+import { resetGameData } from "./src/utils.js";
+import limitPage from "./src/limitPage.js";
 
 const startButton = document.querySelector('.startButton');
 const puzzleContainer = document.querySelector('.container');
 const changeImageButton = document.querySelector('#changeImage');
-const chioceSection = document.querySelector('.choice-screen')
-const gameScreen = document.querySelector('.game-screen');
-    
-const limitTimeArray = [10, 20, 30, 40];
-    limitTimeArray.map((item) => {
-    const limitButton = document.createElement('button');
-    limitButton.textContent = item;
-        limitButton.addEventListener('click', function () {
-        chioceSection.classList.add('hide');
-        gameScreen.classList.remove('hide');
-            gameData.limitTime = item;    
-            console.log(gameData.limitTime);
-    })
-    
-    chioceSection.appendChild(limitButton);
-
-
-})
-
-
+const choiceSection = document.querySelector('.choice-screen')
 
 startButton.addEventListener('click', function () {
+    limitPage();
     resetGameData(gameData);
-    createInterval(gameData);
+
     
-    chioceSection.classList.remove('hide');
+    choiceSection.classList.remove('hide');
 
     const startScreen = document.querySelector('.start-screen');
     startScreen.classList.add('hide');
 
     gameData.changeOriginalImageset();
     gameData.generateImageIndexes();
-
 
     if (!document.getElementById(1)) {
         for (let i = 0; i < 9; i++) {
---
+++ src/limitPage.js
@@ -0,0 +1,30 @@
+
+import { createInterval } from "./utils.js";
+import gameData from "./gameData.js";
+
+export default function limitPage() {
+    const gameScreen = document.querySelector('.game-screen');
+    const choiceSection = document.querySelector('.choice-screen')
+    const limitTimeArray = [10, 20, 30];
+    const limitButtonWrapper = document.createElement('div');
+    limitButtonWrapper.classList.add('limit-button-wrapper')
+    const limitContent = document.createElement('span');
+    limitContent.textContent = "원하시는 제한시간을 설정해주세요.";
+    choiceSection.appendChild(limitContent);
+    choiceSection.appendChild(limitButtonWrapper);
+
+    limitTimeArray.map((item) => {
+        const limitButton = document.createElement('button');
+        limitButton.textContent = item;
+        limitButton.addEventListener('click', function () {
+            choiceSection.classList.add('hide');
+            gameScreen.classList.remove('hide');
+            gameData.limitTime = item;   
+            createInterval(gameData);
+        })
+    limitButtonWrapper.appendChild(limitButton);
+    })
+    return choiceSection;
+}
+
+
